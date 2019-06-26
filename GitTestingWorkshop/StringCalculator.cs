c5da7aafce90be4bdfb133384501a72d949cab2f
jacekkuch <45226035+jacekkuch@users.noreply.github.com>
2019-06-26 18:26:02 +0200

    Added basic add method.

--- GitTestingWorkshop/StringCalculator.cs
+++ GitTestingWorkshop/StringCalculator.cs
@@ -2,6 +2,9 @@
 {
     public class StringCalculator
     {
-        
+        public int Add(string val1, string val2)
+        {
+            return int.Parse(val1) + int.Parse(val2);
+        }
     }
 }
