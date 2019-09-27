6402564b56e4eb3d5253719ce33dda339079dae6
jacekkuch <45226035+jacekkuch@users.noreply.github.com>
2019-09-27 21:48:14 +0200

    my

--- GitTestingWorkshop.Tests.Unit/StringCalculatorTests.cs
+++ GitTestingWorkshop.Tests.Unit/StringCalculatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shouldly;
+using System;
 
 namespace GitTestingWorkshop.Tests.Unit
 {
@@ -7,9 +8,60 @@ namespace GitTestingWorkshop.Tests.Unit
     public class StringCalculatorTests
     {
         [TestMethod]
-        public void Test()
+        public void Add_GivenNoNumbers_ReturnsZero()
         {
-            (null as object).ShouldBeNull();
+            AssertSum("", 0);
+        }
+
+        [TestMethod]
+        public void Add_GivenOneNumber_ReturnsTheNumber()
+        {
+            AssertSum("1", 1);
+        }
+
+        [TestMethod]
+        public void Add_GivenTwoNumber_ReturnsTheSum()
+        {
+            AssertSum("1,2", 3);
+        }
+
+        [TestMethod]
+        public void Add_GivenManyNumbers_ReturnsTheSum()
+        {
+            AssertSum("1,2,3,4,5", 15);
+        }
+
+        private class RecordTest
+        {
+            public string ID { get; set; }
+            public string Title { get; set; }
+            public DateTime? Date { get; set; }
+            public Guid? Guid { get; set; }
+            public string[] Properties { get; set; }
+
+        }
+
+        [TestMethod]
+        public void GetJson_GivenObjectWithNulledProperties_ReturnsValidJsonWithNoNulledProperties()
+        {
+            var calc = new StringCalculator();
+            var pinky = new RecordTest
+            {
+                ID = "1",
+                Title = "And his fellow Brain.",
+                Date = null,
+                Guid = null
+            };
+
+            var result = calc.GetJson(pinky);
+            result.ShouldBe("{\"ID\":\"1\",\"Title\":\"And his fellow Brain.\"}");
+        }
+
+        private static void AssertSum(string numbers, int expected)
+        {
+            var calc = new StringCalculator();
+            var result = calc.Add(numbers);
+            result.ShouldBe(expected);
         }
     }
 }
--- GitTestingWorkshop/StringCalculator.cs
+++ GitTestingWorkshop/StringCalculator.cs
@@ -1,7 +1,23 @@
-﻿namespace GitTestingWorkshop
+﻿using Newtonsoft.Json;
+using System.Linq;
+
+namespace GitTestingWorkshop
 {
     public class StringCalculator
     {
-        public int Add(string val1, string val2) => int.Parse(val1) + int.Parse(val2);
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrWhiteSpace(numbers))
+                return 0;
+            return numbers.Split(',').Select(int.Parse).Sum();
+        }
+
+        public string GetJson<T>(T myObject)
+        {
+            var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.None };
+            var myObjectSerialized = JsonConvert.SerializeObject(myObject, settings);
+
+            return myObjectSerialized;
+        }
     }
 }
